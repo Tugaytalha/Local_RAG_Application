74f79efb6101d758e95ce450c741524fa85cc2ab
Tugaytalha <mukoicen@gmail.com>
2024-12-27 15:29:42 +0300

    USe local-embeddings, update requirements and gitignore

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,10 +1,6 @@
 from langchain_community.embeddings.ollama import OllamaEmbeddings
-from langchain_community.embeddings.bedrock import BedrockEmbeddings
-
 
 def get_embedding_function():
-    embeddings = BedrockEmbeddings(
-        credentials_profile_name="default", region_name="us-east-1"
-    )
-    # embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    # Use the local nomic-embed-text-v1.5 model
+    embeddings = OllamaEmbeddings(model="nomic-embed-text-v1.5", run_local=True)
     return embeddings
